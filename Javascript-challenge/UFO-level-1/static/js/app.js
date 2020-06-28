c71cf73abfe16dc998bf5cc2fcf320b76286d61e
Your Name <you@example.com>
2020-06-28 01:14:10 -0700

    hw

--- Javascript-challenge/UFO-level-1/static/js/app.js
+++ Javascript-challenge/UFO-level-1/static/js/app.js
@@ -2,4 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var filterTable
+var filterTableButton = d3.select("#filter-btn");
+
+var tbody = d3.select("tbody");
+
+var queryDate = "tbd";
+
+filterTableButton.on("click", function () {
+
+    d3.event.preventDefault();
+
+    var input = d3.select("#datetime");
+
+    var queryDate = input.property('value');
+
+    var filterData = tableData.filter(function (data) {
+        return data.datetime == queryDate;
+    });
+
+    tbody.text('');
+
+    filterData.forEach(record => {
+        var row = tbody.append('tr');
+
+        Object.entries(record).forEach(([key, value]) => {
+
+            row.append('td').text(value);
+        });
+    });
+
+});
